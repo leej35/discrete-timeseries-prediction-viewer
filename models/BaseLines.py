0ec84ff8fb5b1725da11c5110dfcc103cc4f5e38
leej35 <thelab@naver.com>
2017-11-18 22:54:43 -0500

    Update BaseLines.py

--- models/BaseLines.py
+++ models/BaseLines.py
@@ -6,9 +6,11 @@ class BaseLines(object):
 		self.p_c_given_t = (tc_freq.astype(float)+1) / (self.row_sums[:, np.newaxis] + 1 + corpus_size)
 		# get probability of target <prior>
 		self.p_t = t_freq.astype(float) / sum(t_freq)
+		
 	## Type0: prior probability :: same as returning p_t
 	def prior(self):
 		return self.p_t
+
 	## Type1: consider only observed in the context
 	def nb_pred_t1(self, context_ids):
 		target_prob = np.zeros((self.p_t.shape))
@@ -27,25 +29,7 @@ class BaseLines(object):
 			target_prob[target_id] = math.exp(log_sum_xj) * self.p_t[target_id]
 		target_prob = target_prob / sum(target_prob)
 		return target_prob
-	## Type2: consider both observed and unobserved
-	# it is much slower than Type1
-	def nb_pred_t2(self, context_ids):
-		target_prob = np.zeros((self.p_t.shape))
-		for target_id in range(corpus_size):
-			# skip if the target has not seen in training set
-			if self.row_sums[target_id] == 0:
-				continue
-			log_sum_xj = 0
-			for context_id in range(corpus_size):
-				if context_id in context_ids:
-					log_sum_xj += math.log(self.p_c_given_t[target_id, context_id])
-				else:
-					if self.p_c_given_t[target_id, context_id] == 0:
-						continue
-					log_sum_xj += math.log(1 - self.p_c_given_t[target_id, context_id])
-			target_prob[target_id] = math.exp(log_sum_xj) * self.p_t[target_id]
-		target_prob = target_prob / sum(target_prob)
-		return target_prob
+
 	## Type3: randomly pick any
 	def random_pred(self):
 		pred = np.random.rand(corpus_size,1)[:,0]
